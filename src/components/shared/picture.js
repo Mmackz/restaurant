a9438cda88920f1e05c572e879f200b92b467364
Mmackz <matthewmackenz@gmail.com>
2022-05-24 14:02:49 -0700

    ♻️ allow for sets of 4 images

--- src/components/shared/picture.js
+++ src/components/shared/picture.js
@@ -1,19 +1,36 @@
 export const makeImage = (images, alt, className = "card-image") => {
    const picture = document.createElement("picture");
-   picture.innerHTML = `
-      <source
-         media="(max-width: 499.98px)"
-         srcset="${images[0]} 1x, ${images[1]} 2x"
-      />
-      <source
-         media="(max-width: 767.98px)"
-         srcset="${images[2]} 1x, ${images[3]} 2x"
-      />
-      <source
-         media="(min-width: 768px)"
-         srcset="${images[4]} 1x, ${images[5]} 2x"
-      />
-      <img class=${className} src="${images[0]}" alt="${alt}" width="100%" />
-   `;
+
+   if (images.length === 4) {
+      picture.innerHTML = `
+         <source
+            media="(max-width: 767.98px)"
+            srcset="${images[0]} 1x, ${images[1]} 2x"
+         />
+         <source
+            media="(min-width: 768px)"
+            srcset="${images[2]} 1x, ${images[3]} 2x"
+         />
+         <img class=${className} src="${images[0]}" alt="${alt}" width="100%" />
+      `;
+   }
+
+   if (images.length === 6) {
+      picture.innerHTML = `
+         <source
+            media="(max-width: 499.98px)"
+            srcset="${images[0]} 1x, ${images[1]} 2x"
+         />
+         <source
+            media="(max-width: 767.98px)"
+            srcset="${images[2]} 1x, ${images[3]} 2x"
+         />
+         <source
+            media="(min-width: 768px)"
+            srcset="${images[4]} 1x, ${images[5]} 2x"
+         />
+         <img class=${className} src="${images[0]}" alt="${alt}" width="100%" />
+      `;
+   }
    return picture;
 };
