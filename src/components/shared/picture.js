669f013434d24c375feb7ae579f4268d45b8a8b8
Mmackz <matthewmackenz@gmail.com>
2022-05-24 17:30:06 -0700

    🎨 change width for desktop media query

--- src/components/shared/picture.js
+++ src/components/shared/picture.js
@@ -22,11 +22,11 @@ export const makeImage = (images, alt, className = "card-image") => {
             srcset="${images[0]} 1x, ${images[1]} 2x"
          />
          <source
-            media="(max-width: 767.98px)"
+            media="(max-width: 899.98px)"
             srcset="${images[2]} 1x, ${images[3]} 2x"
          />
          <source
-            media="(min-width: 768px)"
+            media="(min-width: 900px)"
             srcset="${images[4]} 1x, ${images[5]} 2x"
          />
          <img class=${className} src="${images[0]}" alt="${alt}" width="100%" />
