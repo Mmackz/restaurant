4527ce295af53390fabf40d17b74523a698c7280
Mmackz <matthewmackenz@gmail.com>
2022-05-28 21:52:05 -0700

    🚧 layout/markup for gallery section

---
+++ src/components/home/gallery/index.js
@@ -0,0 +1,60 @@
+import "./gallery.css";
+import { makeImage } from "/src/components/shared/picture.js";
+import {
+   familyGatheringDesktop,
+   familyGatheringDesktop2x,
+   familyGatheringTablet,
+   familyGatheringTablet2x,
+   familyGatheringMobile,
+   familyGatheringMobile2x
+} from "/src/images";
+
+export const Gallery = (() => {
+   // create the gallery section
+   const section = document.createElement("section");
+   section.classList.add("gallery");
+
+   // create a container for the gallery images
+   const imageContainer = document.createElement("div");
+   imageContainer.setAttribute("id", "gallery-image");
+   imageContainer.classList.add("gallery-image-container");
+
+   // set default image
+   const defaultImage = makeImage(
+      [familyGatheringMobile,
+      familyGatheringMobile2x,
+      familyGatheringTablet,
+      familyGatheringTablet2x,
+      familyGatheringDesktop,
+      familyGatheringDesktop2x],
+      "Family Gathering"
+   );
+
+   imageContainer.appendChild(defaultImage);
+   section.appendChild(imageContainer);
+
+   // create list of choices for gallery images
+   const galleryItems = document.createElement("ul");
+   galleryItems.classList.add("gallery-items");
+   galleryItems.innerHTML = `
+      <li class="gallery-item" data-item="1">Family Gathering</li>
+      <li class="gallery-item" data-item="2">Special Events</li>
+      <li class="gallery-item" data-item="3">Social Events</li>
+   `;
+
+   // create a container for the gallery text content
+   const galleryContent = document.createElement("div");
+   galleryContent.setAttribute("id", "gallery-text");
+   galleryContent.classList.add("gallery-content", "card-content");
+
+   galleryContent.innerHTML = `
+      <h3 class="heading-l card-heading">Family Gathering</h3>
+      <p class="text-lg card-text">We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.</p>
+      <button class="button button-light hero-button heading-s">Book A Table</button>
+   `;
+
+   galleryContent.prepend(galleryItems);
+   section.appendChild(galleryContent);
+
+   return section;
+})();
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,13 @@
 import { Hero } from "./hero";
 import { Info } from "./info";
 import { Menu } from "./menu";
+import { Gallery } from "./gallery";
 
 export default (() => {
    const main = document.createElement("main");
    main.appendChild(Hero);
    main.appendChild(Info);
    main.appendChild(Menu);
+   main.appendChild(Gallery);
    return main;
 })();
