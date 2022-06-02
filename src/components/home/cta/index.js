183a86540e25667e36f442306bd8c581f0298244
Mmackz <62824345+Mmackz@users.noreply.github.com>
2022-06-02 20:36:16 +0000

    ✨ cta section

---
+++ src/components/home/cta/index.js
@@ -0,0 +1,47 @@
+import "./cta.css";
+import { makeImage } from "/src/components/shared/makeImage";
+import {
+   readyBgDesktop,
+   readyBgDesktop2x,
+   readyBgTablet,
+   readyBgTablet2x,
+   readyBgMobile,
+   readyBgMobile2x
+} from "/src/images";
+
+export const Cta = (() => {
+   // create the cta section
+   const section = document.createElement("section");
+   section.classList.add("cta");
+
+   // insert background image
+   const background = makeImage(
+      [
+         readyBgMobile,
+         readyBgMobile2x,
+         readyBgTablet,
+         readyBgTablet2x,
+         readyBgDesktop,
+         readyBgDesktop2x
+      ],
+      "Pasta dish",
+      ["cta-image"]
+   );
+   section.append(background);
+
+   // create inner container
+   const container = document.createElement("div");
+   container.classList.add("cta-inner");
+
+   const heading = document.createElement("h2");
+   heading.classList.add("cta-heading", "heading-l");
+   heading.textContent = "Ready to make a reservation?";
+
+   const button = document.createElement("button");
+   button.classList.add("cta-button", "button", "button-dark", "heading-s");
+   button.textContent = "Book A Table";
+
+   container.append(heading, button);
+   section.append(container);
+   return section;
+})();
