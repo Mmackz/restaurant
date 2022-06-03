6177a7a5fa6b31545220c0ce17e3c72ef7d484e0
Mmackz <62824345+Mmackz@users.noreply.github.com>
2022-06-03 01:23:35 +0000

    ♻️ rename images to fix conflict

--- src/components/booking/images/index.js
+++ src/components/booking/images/index.js
@@ -1,6 +1,6 @@
-export { default as heroBgDesktop } from "./hero-bg-desktop.jpg";
-export { default as heroBgDesktop2x } from "./hero-bg-desktop@2x.jpg";
-export { default as heroBgMobile } from "./hero-bg-mobile.jpg";
-export { default as heroBgMobile2x } from "./hero-bg-mobile@2x.jpg";
-export { default as heroBgTablet } from "./hero-bg-tablet.jpg";
-export { default as heroBgTablet2x } from "./hero-bg-tablet@2x.jpg";
+export { default as BgDesktop } from "./bg-desktop.jpg";
+export { default as BgDesktop2x } from "./bg-desktop@2x.jpg";
+export { default as BgMobile } from "./bg-mobile.jpg";
+export { default as BgMobile2x } from "./bg-mobile@2x.jpg";
+export { default as BgTablet } from "./bg-tablet.jpg";
+export { default as BgTablet2x } from "./bg-tablet@2x.jpg";
