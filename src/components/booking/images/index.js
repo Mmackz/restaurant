87f59057b9dcc17bfba46cb126881a0791c179c9
Mmackz <matthewmackenz@gmail.com>
2022-06-02 17:17:38 -0700

    ➕ add booking images folder

---
+++ src/components/booking/images/index.js
@@ -0,0 +1,6 @@
+export { default as heroBgDesktop } from "./hero-bg-desktop.jpg";
+export { default as heroBgDesktop2x } from "./hero-bg-desktop@2x.jpg";
+export { default as heroBgMobile } from "./hero-bg-mobile.jpg";
+export { default as heroBgMobile2x } from "./hero-bg-mobile@2x.jpg";
+export { default as heroBgTablet } from "./hero-bg-tablet.jpg";
+export { default as heroBgTablet2x } from "./hero-bg-tablet@2x.jpg";
