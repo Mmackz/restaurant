1e2bbd1b20f753f14547f210d8277ff01514bc67
Mmackz <matthewmackenz@gmail.com>
2022-06-07 20:12:46 -0700

    ♿️ add aria-labels to guests input

--- src/components/booking/form/guests-input.js
+++ src/components/booking/form/guests-input.js
@@ -8,11 +8,14 @@ export const GuestsInput = (() => {
       // create input for number of guests
       const guestsInput = document.createElement("div");
       guestsInput.classList.add("booking-input", "guests-input");
+      guestsInput.setAttribute("aria-label", "select number of guests");
    
       const guests = document.createElement("div");
       guests.classList.add("guests");
+      guest.setAttribute("aria-label", "number of guests");
    
       const guestsNumber = document.createElement("span");
+      guestsNumber.setAttribute("id", "guests");
       guestsNumber.innerText = guestsAmount;
    
       const guestsSuffix = document.createElement("span");
@@ -22,11 +25,13 @@ export const GuestsInput = (() => {
       const addBtn = document.createElement("button");
       addBtn.classList.add("guests-input-btn");
       addBtn.setAttribute("type", "button");
+      addBtn.setAttribute("aria-label", "add guest");
       addBtn.innerHTML = `<img src=${iconPlus} alt="add guest">`;
    
       const minusBtn = document.createElement("button");
       minusBtn.classList.add("guests-input-btn", "minus");
       minusBtn.setAttribute("type", "button");
+      minusBtn.setAttribute("aria-label", "remove guest");
       minusBtn.innerHTML = `<img src=${iconMinus} alt="remove guest">`;
 
       addBtn.addEventListener("click", () => updateGuestsNumber(1));
