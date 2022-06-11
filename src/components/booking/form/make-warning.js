aeb89b4d0cd693c85004d3428e918cf5077dd56e
Mmackz <matthewmackenz@gmail.com>
2022-06-11 13:35:24 -0700

    ✨ add dist subtree for deployment

--- src/components/booking/form/make-warning.js
+++
@@ -1,9 +0,0 @@
-export const makeWarning = (message) => {
-   const nameValidationWarning = document.createElement("div");
-   nameValidationWarning.classList.add("validation-warning");
-   const warningText = document.createElement("p");
-   warningText.classList.add("warning-text");
-   warningText.textContent = message;
-   nameValidationWarning.appendChild(warningText);
-   return nameValidationWarning;
-};
--- src/components/booking/images/index.js
+++
@@ -1,6 +0,0 @@
-export { default as BgDesktop } from "./bg-desktop.jpg";
-export { default as BgDesktop2x } from "./bg-desktop@2x.jpg";
-export { default as BgMobile } from "./bg-mobile.jpg";
-export { default as BgMobile2x } from "./bg-mobile@2x.jpg";
-export { default as BgTablet } from "./bg-tablet.jpg";
-export { default as BgTablet2x } from "./bg-tablet@2x.jpg";
